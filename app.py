c980debeba22608b3e4f5ff854398cbf2e299ffe
Abdul <abdelrahmanelsadig1@gmail.com>
2022-01-03 16:26:11 +0200

    init

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, url_for,flash,session,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import os
 app = Flask(__name__)
-app.config['SQLACHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///blog_post.db'
+SECRET_KEY = '!AweSomeNess150'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
